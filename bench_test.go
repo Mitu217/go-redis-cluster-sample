ad3f4d210244d98cd2c67f5ea6f142df99c76e35
Mitu217 <mtksk.26884um@gmail.com>
2019-10-12 16:47:53 +0900

    add bench large args

--- bench_test.go
+++ bench_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"runtime"
+	"strings"
 	. "testing"
 
 	"github.com/Mitu217/go-redis-cluster-sample/goredis"
@@ -8,6 +10,8 @@ import (
 )
 
 func BenchmarkSerialGetSet(b *B) {
+	key := "foo"
+	val := "bar"
 	hosts := []string{
 		"127.0.0.1:7000",
 		"127.0.0.1:7001",
@@ -22,7 +26,7 @@ func BenchmarkSerialGetSet(b *B) {
 
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			if err := cli.SetGet("foo", "bar"); err != nil {
+			if err := cli.SetGet(key, val); err != nil {
 				b.Fatal(err)
 			}
 		}
@@ -36,9 +40,48 @@ func BenchmarkSerialGetSet(b *B) {
 
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			if err := cli.SetGet("foo", "bar"); err != nil {
+			if err := cli.SetGet(key, val); err != nil {
 				b.Fatal(err)
 			}
 		}
 	})
 }
+
+func BenchmarkSerialGetSetLargeArgs(b *B) {
+	key := strings.Repeat("foo", 24)
+	val := strings.Repeat("bar", 4096)
+	hosts := []string{
+		"127.0.0.1:7000",
+		"127.0.0.1:7001",
+		"127.0.0.1:7002",
+	}
+
+	b.Run("radix", func(b *B) {
+		cli, err := radix.NewRadixClient(hosts)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			if err := cli.SetGet(key, val); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	b.Run("go-redis", func(b *B) {
+		cli, err := goredis.NewGoRedisClient(hosts)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			if err := cli.SetGet(key, val); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
+
